4c344c6d6f3b0cb6adb5ed78a89f5d03077fa2d9
Shiyu-D <barbaradsy@gmail.com>
2019-04-21 13:43:53 +1000

    changed server main

--- src/unimelb/bitbox/ServerMain.java
+++ src/unimelb/bitbox/ServerMain.java
@@ -2,16 +2,19 @@ package unimelb.bitbox;
 
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 import java.util.logging.Logger;
 
 import unimelb.bitbox.util.Configuration;
 import unimelb.bitbox.util.FileSystemManager;
 import unimelb.bitbox.util.FileSystemObserver;
 import unimelb.bitbox.util.FileSystemManager.FileSystemEvent;
+import unimelb.bitbox.PeerConnection;
 
 public class ServerMain implements FileSystemObserver {
 	private static Logger log = Logger.getLogger(ServerMain.class.getName()); 
 	protected FileSystemManager fileSystemManager;
+	private ArrayList<PeerConnection> connections = new ArrayList<>();
 	
 	public ServerMain() throws NumberFormatException, IOException, NoSuchAlgorithmException {
 		fileSystemManager=new FileSystemManager(Configuration.getConfigurationValue("path"),this);
@@ -21,6 +24,10 @@ public class ServerMain implements FileSystemObserver {
 	@Override
 	public void processFileSystemEvent(FileSystemEvent fileSystemEvent) {
 		// TODO: process events
+		for (PeerConnection connection:connections)
+		{
+
+		}
 		//System.out.println("lol");
 	}
 	
