01fd09643e03c1425ad4233a7037c6cdc653c13d
Derekxuan <derekxuan@outlook.com>
2019-05-20 12:44:31 +1000

    still doing!

--- src/unimelb/bitbox/Encryption.java
+++ src/unimelb/bitbox/Encryption.java
@@ -1,37 +1,47 @@
 package unimelb.bitbox;
 
+import com.sun.javafx.scene.traversal.Algorithm;
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
 import unimelb.bitbox.util.Configuration;
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
-import java.security.Key;
+import java.security.*;
 import java.io.UnsupportedEncodingException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import javax.crypto.SecretKey;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.logging.Logger;
 
 public class Encryption {
 	private static Logger log = Logger.getLogger(Encryption.class.getName());
 	static String encryptSharedKey(String identity) throws Exception {
+		//do i need code the Json message sending part here or this part may
+		//return more than one parameter like a status about if this works
 		//TODO generate a shared key, encrypt the shared key with the public key of the associated identity and encode it as base 64
+		Key publicKey=null;
 		String LocalKey = Configuration.getConfigurationValue("identity");
 		if(LocalKey.contains(identity)){
 			String[] substr = LocalKey.split(",");
 			for (String s : substr) {
 				if (s.contains(identity)) {
 					String[] identityKey = s.split(" ");
-					String publicKey = identityKey[identityKey.length - 2];
+					publicKey = getPublicKey(identityKey[identityKey.length - 2]);
 				}
 			}
 			KeyGenerator kg = KeyGenerator.getInstance("AES");
 			kg.init(128);
 			SecretKey secretKey = kg.generateKey();
 			byte[] bK = secretKey.getEncoded();
-			String sharedKey = java.util.Base64.getEncoder().encodeToString(bK);
+			//String sharedKey = java.util.Base64.getEncoder().encodeToString(bK);
 //			String encryptSharedKey;
-			return "encryptSharedKey";
+
+			Cipher cipher = Cipher.getInstance("RSA");
+			//加密模式
+			cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+			String encryptedSharedKey = java.util.Base64.getEncoder().encodeToString(cipher.doFinal(bK));
+			return encryptedSharedKey;
 		}
 		else{
 			log.info("No such a public Key");
@@ -43,19 +53,52 @@ public class Encryption {
 	
 	static String decryptSharedKey(String encryptedKey){
 		//TODO decode the encrypted key as base 64, decrypt the key with your private key stored in bitboxclient_rsa
+		//byte[] encryptedMsg =
 		return "decryptedSharedKey";
 	}
 	
-	static String encryptMessage(String msg, String sharedKey){
+	static String encryptMessage(String msg, Key sharedKey){
 		//TODO encrypt the message with the shared key
-		String encryptedMsg = msg;
-		return encryptedMsg;
+		try{
+			Cipher cipher = Cipher.getInstance("AES");//java默认"RSA"="RSA/ECB/PKCS1Padding"
+			cipher.init(Cipher.ENCRYPT_MODE, sharedKey);
+			byte[] output = cipher.doFinal(msg.getBytes());
+			String encrypteMsg = java.util.Base64.getEncoder().encodeToString(output);
+			return encrypteMsg;
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+		return null;
+	}
+//		String encryptedMsg = msg;
+//		return encryptedMsg;
 	}
 	
-	static String decryptMessage(String encryptedMsg, String sharedKey){
+	static String decryptMessage(String encryptedMsg, Key sharedKey){
 		//TODO decrypt the message with the shared key
-		String msg = encryptedMsg;
-		return msg;
+		try {
+			Cipher cipher = Cipher.getInstance("AES");
+			cipher.init(Cipher.DECRYPT_MODE, sharedKey);
+			BASE64Decoder decoder = new BASE64Decoder();
+			byte[] c = decoder.decodeBuffer(encryptedMsg);
+			byte[] result = cipher.doFinal(c);
+			String plainText = new String(result, "UTF-8");
+			return plainText;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+//		String msg = encryptedMsg;
+//		return msg;
+	}
+
+	static PublicKey getPublicKey(String key) throws Exception {
+		//TODO getPublicKey from a string read from the configuration file
+		byte[] keyBytes;
+		keyBytes = (new BASE64Decoder()).decodeBuffer(key);
+		X509EncodedKeySpec keySpec = new X509EncodedKeySpec(keyBytes);
+		KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+		PublicKey publicKey = keyFactory.generatePublic(keySpec);
+		return publicKey;
 	}
 	
 }
