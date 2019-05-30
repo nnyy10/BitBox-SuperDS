259428f7f80cb0e71b841d0e43fc6369e6b5c114
Derekxuan <derekxuan@outlook.com>
2019-05-30 19:45:57 +1000

    encryption part is done!

--- src/unimelb/bitbox/Encryption.java
+++ src/unimelb/bitbox/Encryption.java
@@ -1,20 +1,26 @@
 package unimelb.bitbox;
 import com.google.common.io.ByteStreams;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.openssl.PEMKeyPair;
+import org.bouncycastle.openssl.PEMParser;
 import unimelb.bitbox.util.Configuration;
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import java.io.*;
 import java.math.BigInteger;
-import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import java.security.*;
 import javax.crypto.SecretKey;
-import java.security.interfaces.RSAPrivateKey;
-import java.security.spec.RSAPublicKeySpec;
-import java.security.spec.X509EncodedKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.spec.*;
 import java.util.Arrays;
 import java.util.Base64;
+import java.util.Scanner;
 import java.util.logging.Logger;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.security.Security;
+
 
 import static com.google.common.base.Preconditions.checkArgument;
 
@@ -22,40 +28,30 @@ public class Encryption {
 	private static final String SSH_MARKER = "ssh-rsa";
 
 	private static Logger log = Logger.getLogger(Encryption.class.getName());
+
 	static String encryptSharedKey(String identity) throws Exception {
-		//do i need code the Json message sending part here or this part may
-		//return more than one parameter like a status about if this works
 		//TODO generate a shared key, encrypt the shared key with the public key of the associated identity and encode it as base 64
-		PublicKey publicKey=null;
-		String LocalKey = Configuration.getConfigurationValue("identity");
-		if(LocalKey.contains(identity)){
-			String[] substr = LocalKey.split(",");
-			for (String s : substr) {
-				if (s.contains(identity)) {
-					String identityKey = s;
-					//publicKey = getPublicKey(identityKey[-2：-1]);
-				}
-			}
+		try {
+			PublicKey publicKey= getPublicKey(identity);
+			String LocalKey = Configuration.getConfigurationValue("identity");
 			KeyGenerator kg = KeyGenerator.getInstance("AES");
 			kg.init(128);
 			SecretKey secretKey = kg.generateKey();
 			byte[] bK = secretKey.getEncoded();
-			//String sharedKey = java.util.Base64.getEncoder().encodeToString(bK);
-//			String encryptSharedKey;
+			log.info("sent: " + Arrays.toString(bK));
 
 			Cipher cipher = Cipher.getInstance("RSA");
-			//加密模式
+			//encrypt mode
 			cipher.init(Cipher.ENCRYPT_MODE, publicKey);
 			String encryptedSharedKey = java.util.Base64.getEncoder().encodeToString(cipher.doFinal(bK));
 			return encryptedSharedKey;
 		}
-		else{
-			log.info("No such a public Key");
-			//JSON_process.AUTH_RESPONSE(false," ");
-			return null;
+		catch (Exception e){
+			log.info("Encrypt shared key fail");
 		}
-
+		return null;
 	}
+
 	
 	static String decryptSharedKey(String encryptedKey,String path) throws Exception {
 		//TODO decode the encrypted key as base 64, decrypt the key with your private key stored in bitboxclient_rsa
@@ -68,15 +64,17 @@ public class Encryption {
 			//String sharedKey = java.util.Base64.getEncoder().encodeToString(output);
 			return sharedKey;
 		}
-		catch (Exception e){
+		catch (Exception e) {
 			e.printStackTrace();
 			return null;
 		}
-		//return "decryptedSharedKey";
 	}
 	
-	static String encryptMessage(String msg, Key sharedKey){
+	static String encryptMessage(String msg, String str){
 		//TODO encrypt the message with the shared key
+		String[] byteValues = str.substring(1, str.length() - 1).split(",");
+		byte[] bytes = new byte[byteValues.length];
+		Key sharedKey = new SecretKeySpec(bytes, 0, bytes.length,"AES");
 		try{
 			Cipher cipher = Cipher.getInstance("AES");//java默认"RSA"="RSA/ECB/PKCS1Padding"
 			cipher.init(Cipher.ENCRYPT_MODE, sharedKey);
@@ -87,13 +85,13 @@ public class Encryption {
 			e.printStackTrace();
 			return null;
 		}
-		//return "encryptedMsg";
 	}
-//		String encryptedMsg = msg;
-//		return encryptedMsg;
 	
-	static String decryptMessage(String encryptedMsg, Key sharedKey){
+	static String decryptMessage(String encryptedMsg, String str){
 		//TODO decrypt the message with the shared key
+		String[] byteValues = str.substring(1, str.length() - 1).split(",");
+		byte[] bytes = new byte[byteValues.length];
+		Key sharedKey = new SecretKeySpec(bytes, 0, bytes.length,"AES");
 		try {
 			Cipher cipher = Cipher.getInstance("AES");
 			cipher.init(Cipher.DECRYPT_MODE, sharedKey);
@@ -105,8 +103,6 @@ public class Encryption {
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
-//		String msg = encryptedMsg;
-//		return msg;
 	}
 
 	private static PublicKey getPublicKey(String identity) throws Exception {
@@ -127,10 +123,9 @@ public class Encryption {
 		}
 
 		if(!foundIdentity){
+			log.info("identity not found!");
 			return null;
 		}
-
-
 
 		try {
 			InputStream stream = new ByteArrayInputStream(Base64.getDecoder().decode(RSApublicKeyString));
@@ -145,24 +140,7 @@ public class Encryption {
 		} catch (Exception ex) {
 			throw new RuntimeException(ex);
 		}
-		/*System.out.println(RSApublicKeyString);
-		try{
-			byte[] byteKey = Base64.getDecoder().decode(RSApublicKeyString);
-			X509EncodedKeySpec keySpec = new X509EncodedKeySpec(byteKey);
-			KeyFactory keyFactory = KeyFactory.getInstance("RSA");
 
-			PublicKey pkey = keyFactory.generatePublic(keySpec);
-		}
-		catch(Exception e){
-			e.printStackTrace();
-		}
-*/
-/*		byte[] keyBytes;
-		keyBytes = java.util.Base64.getDecoder().decode(key);
-		X509EncodedKeySpec keySpec = new X509EncodedKeySpec(keyBytes);
-		KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-		PublicKey publicKey = keyFactory.generatePublic(keySpec);
-		return publicKey;*/
 	}
 	private static byte[] readLengthFirst(InputStream in) throws IOException {
 		int[] bytes = new int[]{ in.read(), in.read(), in.read(), in.read() };
@@ -177,36 +155,37 @@ public class Encryption {
 		return val;
 	}
 
-    private static RSAPrivateKey getPrivateKey(String filename) throws Exception {
-//		File f = new File(filename);
-//		FileInputStream fis = new FileInputStream(f);
-//		DataInputStream dis = new DataInputStream(fis);
-//		byte[] keyBytes = new byte[(int) f.length()];
-//		dis.readFully(keyBytes);
-//		dis.close();
-//
-//		String temp = new String(keyBytes);
-//		String privKeyPEM = temp.replace("-----BEGIN PRIVATE KEY-----\n", "");
-//		privKeyPEM = privKeyPEM.replace("-----END PRIVATE KEY-----", "");
-//		//System.out.println("Private key\n"+privKeyPEM);
-//
-//		Base64 b64 = new Base64();
-//		byte [] decoded = b64.decode(privKeyPEM);
-//
-//		PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(decoded);
-//		KeyFactory kf = KeyFactory.getInstance(algorithm);
-//		return kf.generatePrivate(spec);
-		return null;
-    }
+    private static PrivateKey getPrivateKey(String filename) throws Exception {
+		/**
+		 * Key conversion algorithm found at
+		 * https://stackoverflow.com/questions/3243018/how-to-load-rsa-private-key-from-file
+		 */
+		String keyPath = filename;
+		BufferedReader br = new BufferedReader(new FileReader(keyPath));
+		Security.addProvider(new BouncyCastleProvider());
+		PEMParser pp = new PEMParser(br);
+		PEMKeyPair pemKeyPair = (PEMKeyPair) pp.readObject();
+		pp.close();
+		PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(pemKeyPair.getPrivateKeyInfo().getEncoded());
+		KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+		PrivateKey key = keyFactory.generatePrivate(keySpec);
+		//System.out.println(key);
+		return key;
+	}
+
 
     public static void main(String[] args) throws Exception {
-//		RSAPrivateKey privateKey = getPrivateKey("bitboxclient_rsa");
-//		String privatek = privateKey.toString();
-//		System.out.println(privatek);
-//		String str = encryptSharedKey("derekxuan@outlook.com");
-		Key str = getPublicKey("derekxuan@outlook.com");
-		System.out.println("the key is: " + str + "finished");
-
+		String Msg = "this is a test message";
+		//System.out.println("the key is: " + str + " finished");
+		String s = encryptSharedKey("zhouguozhi@xuandeMacBook-Air.local");
+		//System.out.println(s);
+		//Key pk = getPrivateKey("/");
+		String str = decryptSharedKey(s,"id_rsa");
+		log.info("received: " + str);
+		String TestMsg = encryptMessage(Msg, str);
+		//log.info(TestMsg);
+		String RMsg = decryptMessage(TestMsg, str);
+		System.out.println(RMsg);
 	}
 
 }
