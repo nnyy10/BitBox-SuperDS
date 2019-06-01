f508233bfc2011972e44ab4319ec881294314db2
Naiyun Wu <naiyun1@hotmail.com>
2019-06-01 19:42:29 +1000

    d

--- src/unimelb/bitbox/UDP_entry.java
+++ src/unimelb/bitbox/UDP_entry.java
@@ -117,8 +117,19 @@ public class UDP_entry implements Runnable {
                 }
                 break;
             case "HANDSHAKE_RESPONSE":
+                UDP_peerconnection p = null;
                 for (UDP_peerconnection peer : UDP_peerconnection.waitingForHandshakeConnections) {
                     if (peer.getPort() == receivePort && peer.getInetAddr().equals(receieveAddr)) {
+                        for (UDP_peerconnection.ThreadResponsePair trp : UDP_peerconnection.waitingForResponseThreads) {
+                            System.out.println(trp.addr + ":" + receieveAddr);
+                            System.out.println(trp.port + ":" + receivePort);
+                            System.out.println(JSON_process.RESPONSE_EQUALS(trp.JSON_Response, message));
+                            if (trp.addr.equals(receieveAddr) && trp.port == receivePort && JSON_process.RESPONSE_EQUALS(trp.JSON_Response, message)) {
+                                trp.timer.cancel(); // If there is an active timer thread waiting for this response, stop this timer thread
+                                break;
+                            }
+                        }
+                        p = peer;
                         this.fileSystemObserver.add(peer);
                         UDP_peerconnection.RemovePeerToWaitingList(peer);
                         new Thread(() -> {
@@ -131,6 +142,9 @@ public class UDP_entry implements Runnable {
                         break;
                     }
                 }
+                if (p != null){
+                    UDP_peerconnection.waitingForResponseThreads.remove(p);
+                }
                 break;
             default:
                 for (PeerConnection peer : ServerMain.getInstance().getlist()) {
@@ -139,6 +153,9 @@ public class UDP_entry implements Runnable {
                         if (UDP_peerconnection.isResponseMessage(message)) {
                             UDP_peerconnection.ThreadResponsePair foundThreadPair = null;
                             for (UDP_peerconnection.ThreadResponsePair trp : UDP_peerconnection.waitingForResponseThreads) {
+                                System.out.println(trp.addr + ":" + receieveAddr);
+                                System.out.println(trp.port + ":" + receivePort);
+                                System.out.println(JSON_process.RESPONSE_EQUALS(trp.JSON_Response, message));
                                 if (trp.addr.equals(receieveAddr) && trp.port == receivePort && JSON_process.RESPONSE_EQUALS(trp.JSON_Response, message)) {
                                     trp.timer.cancel(); // If there is an active timer thread waiting for this response, stop this timer thread
                                     foundThreadPair = trp;
